e4f36a7d3dab876e1a2c24e539e59d5b85d33c81
Eric RANDRETSARIMANANA <eric_ra@absys.fr>
2021-11-12 16:59:46 +0100

    maj

--- demo/Film.py
+++ demo/Film.py
@@ -1,25 +1,28 @@
 from flask import request, redirect, Blueprint, render_template, url_for
-from demo.models import Film
+from flask.json import dumps
+from demo.models import Category, Film
 from index import db
 
 film_bp = Blueprint('film', __name__, template_folder='templates', url_prefix='/film')
 
 @film_bp.route('/', methods=['GET'])
 def index():
-    films = Film.query.order_by(Film.id.desc()).all()
+    films = Film.query.join(Category).order_by(Film.id.desc()).all()
     
     return render_template('film/index.html', films=films)
 
 @film_bp.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
-        film = Film(name=request.form['name'], coming_out=request.form['coming_out'])
+        film = Film(name=request.form['name'], coming_out=request.form['coming_out'], category_id=request.form['category_id'])
         
         db.session.add(film)
         db.session.commit()
         return redirect(url_for('film.index'))
 
-    return render_template('film/create.html')
+    categories = Category.query.order_by(Category.id.desc()).all()
+
+    return render_template('film/create.html', categories=categories)
 
 @film_bp.route('/update/<int:id>', methods=('GET', 'POST'))
 def update(id):
@@ -34,8 +37,10 @@ def update(id):
         
         db.session.commit()
         return redirect(url_for('film.index'))
+
+    categories = Category.query.order_by(Category.id.desc()).all()
     
-    return render_template('film/edit.html', film=film)
+    return render_template('film/edit.html', film=film, categories=categories)
 
 @film_bp.route('/delete/<int:id>', methods=['POST'])
 def delete(id):
--- demo/models.py
+++ demo/models.py
@@ -1,4 +1,5 @@
 from index import db 
+from sqlalchemy.orm import relationship
 
 class Category(db.Model):
     __tablename__ = 'category'
@@ -8,16 +9,21 @@ class Category(db.Model):
     
     def __init__(self, category):
         self.category = category
+        
+    def __repr__(self):
+        return '<Category %r>' % self.category
 
 class Film(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     coming_out = db.Column(db.Date)
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category = relationship('Category', back_populates="children")
     
-    def __init__(self, name, coming_out) -> None:
+    def __init__(self, name, coming_out, category_id) -> None:
         self.name = name
         self.coming_out = coming_out
+        self.category_id = category_id
     
     def __repr__(self):
         return '<Film %r>' % self.name
